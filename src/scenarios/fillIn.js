302d140bc401d981286d780d9148075c726728be
Abigail McPhillips <abigail.mcphillips@gmail.com>
2020-10-29 13:43:45 +0000

    wrap text input

--- src/scenarios/fillIn.js
+++ src/scenarios/fillIn.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Text, TextInput } from 'react-native'
-import { useCavy } from 'cavy'
+import { useCavy, wrap } from 'cavy'
 
 export const key = 'FillIn';
 
@@ -11,10 +11,11 @@ const textText = 'text';
 export const Screen = () => {
   const [text, setText] = useState('');
   const generateTestHook = useCavy();
+  const WrappedTextnput = wrap(TextInput);
 
   return (
     <>
-      <TextInput
+      <WrappedTextnput
         ref={generateTestHook(textInputId)}
         value={text}
         onChangeText={value => setText(value)}
